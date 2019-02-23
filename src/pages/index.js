808f3e091c11a32b9d8f8c0469b344bab0c36763
Romel Pérez <ronelprhone@gmail.com>
2019-02-23 17:36:12 -0500

    update: index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,21 +18,25 @@ const styles = theme => {
       width: '100%'
     },
     content: {
-      display: 'block',
+      display: 'flex',
+      flexDirection: 'column',
       margin: [0, 'auto'],
       padding: 20
     },
     brand: {
       margin: [0, 'auto', 30],
       padding: [10, 0],
+      width: '100%',
       maxWidth: 700
     },
     menu: {
       margin: [0, 'auto', 20],
-      maxWidth: 550
+      width: '100%',
+      maxWidth: 600
     },
     social: {
       margin: [0, 'auto'],
+      width: '100%',
       maxWidth: 400
     },
     legal: {
@@ -48,25 +52,53 @@ class Component extends React.Component {
   constructor () {
     super(...arguments);
 
-    this.state = { show: false };
+    this.state = {
+      show1: false,
+      show2: false,
+      show3: false,
+      show4: false
+    };
+
+    this.show3Timeout = null;
+    this.show4Timeout = null;
   }
 
   componentDidMount () {
-    setTimeout(() => this.setState({ show: true }), 1000);
+    setTimeout(() => this.setState({ show1: true }), 1000);
+  }
+
+  componentWillUnmount () {
+    clearTimeout(this.show3Timeout);
+    clearTimeout(this.show4Timeout);
+  }
+
+  onBackgroundEnter = () => {
+    this.setState({ show2: true });
+    this.show3Timeout = setTimeout(() => this.setState({ show3: true }), 50);
+    this.show4Timeout = setTimeout(() => this.setState({ show4: true }), 100);
   }
 
   render () {
-    const { show } = this.state;
+    const { show1, show2, show3, show4 } = this.state;
     const { classes } = this.props;
 
     return (
       <Layout>
-        <Background animation={{ show, duration: { enter: 1000 } }}>
+        <Background animation={{ show: show1 }} onEnter={this.onBackgroundEnter}>
           <div className={classes.root}>
             <div className={classes.content}>
-              <Brand className={classes.brand} />
-              <Menu className={classes.menu} />
-              <SocialLinks className={classes.social} />
+              <Brand
+                className={classes.brand}
+                animation={{ show: show2, independent: true }}
+              />
+              <Menu
+                className={classes.menu}
+                animation={{ show: show3, independent: true }}
+              />
+              <SocialLinks
+                className={classes.social}
+                animation={{ show: show4, duration: { enter: 400 }, independent: true }}
+              />
             </div>
             <Legal className={classes.legal} />
           </div>
