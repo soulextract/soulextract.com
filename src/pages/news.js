a574b0b55e644f0fb5462cd03be55b2c2079d907
Romel Pérez <ronelprhone@gmail.com>
2019-04-19 19:32:43 -0500

    feat: news page

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import { withStyles } from '../tools/withStyles';
+import { Link } from '../components/Link';
 import { Main } from '../components/Main';
+import { Post } from '../components/Post';
 import { Secuence } from '../components/Secuence';
 import { Text } from '../components/Text';
 
@@ -15,14 +18,54 @@ class News extends React.Component {
     classes: PropTypes.object
   };
 
+  constructor () {
+    super(...arguments);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount () {
+    this.loadPosts();
+  }
+
+  loadPosts () {
+    axios
+      .get('/api/facebook/soulextract/posts')
+      .then(response => {
+        const posts = Array.isArray(response.data) ? response.data : [];
+        this.setState({ posts });
+      });
+  }
+
   render () {
     const { classes } = this.props;
+    const { posts } = this.state;
 
     return (
       <Main className={classes.root}>
         <Secuence stagger>
-          <h1><Text>News</Text></h1>
-          <p><Text>Soul Extract latest news posts.</Text></p>
+          <header>
+            <h1><Text>News</Text></h1>
+          </header>
+          {posts.map((post, index) => (
+            <Post
+              key={post.id}
+              audio={{ silent: index > 4 }}
+              data={post}
+            />
+          ))}
+          {!posts.length && (
+            <>
+              <p><Text>No posts were found.</Text></p>
+              <p>
+                <Text>See</Text>
+                {' '}
+                <Link href='https://facebook.com/soulextract/posts' target='facebook'><Text>facebook.com/soulextract.</Text></Link>
+              </p>
+            </>
+          )}
         </Secuence>
       </Main>
     );
---
+++ src/settings/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  origin: 'http://127.0.0.1:14000',
+  protocol: 'http:',
+  host: '127.0.0.1:14000',
+  hostname: '127.0.0.1',
+  port: 14000
+};
