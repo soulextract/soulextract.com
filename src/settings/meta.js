6b32138317fb029a95fbc2bbb12c1fe7f686d339
Romel Pérez <ronelprhone@gmail.com>
2019-05-11 15:39:17 -0500

    update: seo metadata

--- src/settings/meta.js
+++ src/settings/meta.js
@@ -1,11 +1,9 @@
-// TODO: Set up the real domain name on deployment.
-
 module.exports = {
   title: 'Soul Extract',
   description: 'Alternative Rock injected with electronic and cinematic elements. Proud member of the FiXT roster.',
   keywords: 'soul extract, soulextract, alternative electronic rock, cinematic elements, fixt, fixt roster, science fiction',
-  url: 'https://soulextract.netlify.com',
+  url: 'https://soulextract.com',
   twitter: '@soulextract',
-  seoImage: 'https://soulextract.netlify.com/images/soulextract.jpg',
+  seoImage: 'https://soulextract.com/images/soulextract.jpg',
   color: '#000000'
 };
